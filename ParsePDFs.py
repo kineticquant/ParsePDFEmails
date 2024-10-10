7de89a67a8c25206bf1db796499844ee8debf80b
Mike M <51172274+kineticquant@users.noreply.github.com>
2024-10-10 09:41:22 -0400

    Update ParsePDFs.py

--- ParsePDFs.py
+++ ParsePDFs.py
@@ -2,7 +2,7 @@ import re
 import os
 import PyPDF2
 
-def extract_emails_and_orders(text):
+def ext_eml_ord(text):
     email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
     order_pattern = r'Order #(\d+)'
     
@@ -11,14 +11,14 @@ def extract_emails_and_orders(text):
     
     return list(zip(emails, orders))
 
-def search_emails_and_orders_in_pdf(pdf_path):
+def parse_eml_ord_pdf(pdf_path):
     data = []
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
             text = page.extract_text()
-            data += extract_emails_and_orders(text)
+            data += ext_eml_ord(text)
     return data
 
 pdf_directory = r'ENTER YOUR DIRECTORY HERE'
@@ -28,7 +28,7 @@ all_data = []
 for filename in os.listdir(pdf_directory):
     if filename.endswith('.pdf'):
         pdf_path = os.path.join(pdf_directory, filename)
-        data = search_emails_and_orders_in_pdf(pdf_path)
+        data = parse_eml_ord_pdf(pdf_path)
         all_data.extend(data)
 
 for email, order in all_data:
